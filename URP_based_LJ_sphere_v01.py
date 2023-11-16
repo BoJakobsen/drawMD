d6c72f8a5c69996f59a847321e92d3172d174030
Bo Jakobsen <boj@ruc.dk>
2023-11-16 19:58:19 +0100

    Now works and with walls

--- URP_based_LJ_sphere_v01.py
+++ URP_based_LJ_sphere_v01.py
@@ -15,9 +15,10 @@ import matplotlib.pyplot as plt
 RED = (255, 0, 0) 
 BGCOLOR = "purple"
 ATOM_SIZE=10 # pixel diameter for one atom
-MAX_SPEED=50 # in atom diameter per second
+MAX_SPEED=10 # 
 SCREEN_HEIGHT=500
 SCREEN_WIDTH=800
+WALL_THICKNESS=10
 
 rng = np.random.default_rng()
 
@@ -27,12 +28,36 @@ rng = np.random.default_rng()
 class Sim():
     def __init__(self):
         self.eps=1
-
+        self.dt = 0.005*ATOM_SIZE
+        self.eps_wall=10
+        self.x0=WALL_THICKNESS
+        self.y0=WALL_THICKNESS
+        self.x1=SCREEN_WIDTH - WALL_THICKNESS # 
+        self.y1=SCREEN_HEIGHT -WALL_THICKNESS # 
 
     def update_forces(self,atoms):
-        # Lennard-Jones pair forces between particles
+        
         for atom in atoms:
             atom.f = pygame.Vector2((0,0))
+
+            # Repulsive walls
+            tmp = (self.x0-atom.pos.x)*(self.x0-atom.pos.x)
+            tmp *= tmp*tmp
+            atom.f.x += self.eps_wall/tmp
+            tmp = (self.x1-atom.pos.x)*(self.x1-atom.pos.x)
+            tmp *= tmp*tmp
+            atom.f.x -= self.eps_wall/tmp
+            tmp = (self.y0-atom.pos.y)*(self.y0-atom.pos.y)
+            tmp *= tmp*tmp;
+            atom.f.y += self.eps_wall/tmp
+            tmp = (self.y1-atom.pos.y)*(self.y1-atom.pos.y)
+            tmp *= tmp*tmp
+            atom.f.y -= self.eps_wall/tmp      
+	    
+
+
+
+            # Lennard-Jones pair forces between particles
             atoms2=atoms.copy()
             atoms2.remove(atom) # list of other atoms
             for atom2 in atoms2:
@@ -51,15 +76,11 @@ class Sim():
 		
     def update(self,atoms):
         for atom in atoms:
-            atom.pos +=atom.vel * dt
+            atom.pos +=atom.vel * self.dt
         self.update_forces(atoms)
         for atom in atoms: 
-            atom.vel += atom.f / atom.mass * dt
+            atom.vel += atom.f / atom.mass * self.dt
  
-
-
-
-
 class Atom(pygame.sprite.Sprite):
     def __init__(self, color, radius):
         # Call the parent class (Sprite) constructor
@@ -68,8 +89,8 @@ class Atom(pygame.sprite.Sprite):
         self.color=color
 
         #Initial vel, uniform
-        #self.vel=pygame.Vector2.from_polar((rng.uniform(0, MAX_SPEED), rng.uniform(0, 365)))
-        self.vel = pygame.Vector2((0,0))
+        self.vel=pygame.Vector2.from_polar((rng.uniform(0, MAX_SPEED), rng.uniform(0, 365)))
+        #self.vel = pygame.Vector2((0,0))
         # Initial acceleration
         self.a = pygame.Vector2((0,0))
         self.f = pygame.Vector2((0,0))
@@ -108,7 +129,7 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
-dt = 0
+last_draw=0 # time for last draw
 
 
 # some setup for looking at energy
@@ -121,14 +142,13 @@ sim = Sim()
 
 # make walls
 walls=pygame.sprite.Group()
-
-object_=Wall(0, 0, 10, SCREEN_HEIGHT,'green', pygame.Vector2(1,0))
+object_=Wall(0, 0, WALL_THICKNESS, SCREEN_HEIGHT,'green', pygame.Vector2(1,0))
 walls.add(object_)
-object_=Wall(SCREEN_WIDTH-10, 0, SCREEN_WIDTH, SCREEN_HEIGHT,'green', pygame.Vector2(-1,0))
+object_=Wall(SCREEN_WIDTH-WALL_THICKNESS, 0, SCREEN_WIDTH, SCREEN_HEIGHT,'green', pygame.Vector2(-1,0))
 walls.add(object_)
-object_=Wall(10, 0, SCREEN_WIDTH-10, 10,'green', pygame.Vector2(0,-1))
+object_=Wall(WALL_THICKNESS, 0, SCREEN_WIDTH-WALL_THICKNESS, WALL_THICKNESS,'green', pygame.Vector2(0,-1))
 walls.add(object_)
-object_=Wall(10, SCREEN_HEIGHT-10, SCREEN_WIDTH-10, SCREEN_HEIGHT,'green', pygame.Vector2(0,1))
+object_=Wall(WALL_THICKNESS, SCREEN_HEIGHT-WALL_THICKNESS, SCREEN_WIDTH-WALL_THICKNESS, SCREEN_HEIGHT,'green', pygame.Vector2(0,1))
 walls.add(object_)
 
 # Setup a list for the atoms
@@ -140,72 +160,40 @@ new_click=True
 while running:
     
     
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    m_left, _, _ = pygame.mouse.get_pressed()
-    if m_left and new_click:
-        object_ = Atom(RED, ATOM_SIZE) 
-        object_.pos = pygame.mouse.get_pos()
-        atoms.add(object_)
-        new_click=False
-    if not m_left:
-        new_click=True
-
-    sim.update(atoms)    
-    atoms.update() 
-
-    #Check for collition with wall
-    for wall in walls.sprites():
-        #wall_hit_list = pygame.sprite.spritecollide(wall, atoms, True)
-        atom_list = pygame.sprite.spritecollide(wall, atoms, False)
-        for atom in atom_list:
-            atom.vel=atom.vel.reflect(wall.normal)
-                
- 
-    # check for atom colitions
-    #atoms2=atoms.copy()
-    #for atom in atoms:
-    #    atoms2.remove(atom)
-    #    coll_list = pygame.sprite.spritecollide(atom, atoms2, False)
-    #    for atom2 in coll_list:
-            #Hard sphere interactions
-    #        vel1=atom.vel
-    #        vel2=atom2.vel
-    #        atom.vel=vel2
-    #        atom2.vel=vel1
-                    
-        
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill(BGCOLOR)
-
-    # draw atoms and walls
-    walls.draw(screen)
-    atoms.draw(screen) 
     
+    # do the simulation update
+    sim.update(atoms)    
+    
+    # draw if needed
+    if pygame.time.get_ticks()/1000 - last_draw >= 1/50:
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+        last_draw=pygame.time.get_ticks()/1000
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    #Store total Ekin, each 10 frames
-    if kk==10:
-        kk=0
-        ts.append(pygame.time.get_ticks()/1000)
-        Etot=0
-        for atom in atoms:
-            Etot += atom.vel.magnitude()
-        Etots.append(Etot)
-    else:
-        kk += 1
+        m_left, _, _ = pygame.mouse.get_pressed()
+        if m_left and new_click:
+            object_ = Atom(RED, ATOM_SIZE) 
+            object_.pos = pygame.mouse.get_pos()
+            atoms.add(object_)
+            new_click=False
+        if not m_left:
+            new_click=True
 
+        # Update pixel pos
+        atoms.update()     
+
+        # fill the screen with a color to wipe away anything from last frame
+        screen.fill(BGCOLOR)
+        # draw atoms and walls
+        walls.draw(screen)
+        atoms.draw(screen) 
+        # flip() the display to put your work on screen
+        pygame.display.flip()
 
 pygame.quit()
 
